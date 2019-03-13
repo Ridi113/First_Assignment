26981b929f0246fb9c60670db47e251cb3fff2c0
Ridi113 <ridi@cokreates.com>
2019-03-14 04:02:58 +0600

    Assignment 3

--- spring-demo-one/src/com/assignment1/springdemo/TrackCoach.java
+++ spring-demo-one/src/com/assignment1/springdemo/TrackCoach.java
@@ -25,6 +25,14 @@ public class TrackCoach implements Coach {
 	}
 
 
-	
+	// add an init method
+	public void doMyStartupStuff() {
+		System.out.println("TrackCoach: inside method doMyStartupStuff");
+	}
+		
+	// add a destroy method
+	public void doMyCleanupStuffYoYo() {
+		System.out.println("TrackCoach: inside method doMyCleanupStuffYoYo");		
+	}	
 
 }
